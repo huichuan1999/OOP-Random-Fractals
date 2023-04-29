3f1a767889afac6519a3e098307f0518b75aad96
huichuan1999 <58366430+huichuan1999@users.noreply.github.com>
2023-04-29 12:44:40 +0100

    Add Html interaction

--- sketch.js
+++ sketch.js
@@ -1,64 +1,107 @@
 let redSlider, greenSlider, blueSlider, weightSlider, depthSlider;
-let saveSVGButton, saveImageButton, toggleWeightButton;
+let saveSVGButton, saveImageButton, toggleWeightButton, clearModeButron;
 let currentWeight = 1;
 let decreasingWeight = false;
 let angleSlider;
 let angleDisplay;
+let clearEnabled = true;
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight, SVG);
-  //canvas.parent("sketch-container");
+  //canvas.id("canvas");
+  //canvas.parent("sketch-container"); //SVG canvas cannot add into parent, It is weird
 
-  redSlider = select("#redSlider");
-  greenSlider = select("#greenSlider");
-  blueSlider = select("#blueSlider");
-  weightSlider = select("#weightSlider");
-  depthSlider = select("#depthSlider");
+  redSlider = createSlider(0, 255, 0);
+  let redLabel = createP("Red");
+  redLabel.parent("slider-row-1");
+  redSlider.parent("slider-row-1");
 
-  saveSVGButton = select("#saveSVG");
-  saveImageButton = select("#saveImage");
-  toggleWeightButton = select("#toggleWeight");
+  greenSlider = createSlider(0, 255, 0);
+  let greenLabel = createP("Green");
+  greenLabel.parent("slider-row-1");
+  greenSlider.parent("slider-row-1");
+
+  blueSlider = createSlider(0, 255, 0);
+  let blueLabel = createP("Blue");
+  blueLabel.parent("slider-row-1");
+  blueSlider.parent("slider-row-1");
+
+  weightSlider = createSlider(1, 10, 1);
+  let weightLabel = createP("Stroke Weight");
+  weightLabel.parent("slider-row-2");
+  weightSlider.parent("slider-row-2");
+
+  depthSlider = createSlider(1, 10, 6);
+  let depthLabel = createP("Depth");
+  depthLabel.parent("slider-row-2");
+  depthSlider.parent("slider-row-2");
+
+  angleSlider = createSlider(0, TWO_PI, PI / 4, 0.01);
+  let angleLabel = createP("Angle");
+  angleLabel.parent("slider-row-2");
+  angleSlider.parent("slider-row-2");
+
+  saveSVGButton = createButton("Save SVG");
+  saveImageButton = createButton("Save Image");
+  //toggleWeightButton = createButton("Toggle Stroke Weight");
+  clearModeButron = createButton("Clear Mode");
+
+  saveSVGButton.parent("button-container");
+  saveImageButton.parent("button-container");
+  //toggleWeightButton.parent("button-container");
+  clearModeButron.parent("button-container");
+
+  angleDisplay = createSpan();
+  angleDisplay = select("#angleDisplay");
 
   saveSVGButton.mousePressed(exportSVG);
   saveImageButton.mousePressed(() => save("myImage.png"));
+  //toggleWeightButton.mousePressed(() => decreasingWeight = !decreasingWeight);
+  clearModeButron.mousePressed(() => clearEnabled = !clearEnabled);
 
-  angleSlider = createSlider(0, TWO_PI, PI / 4, 0.01);
-  angleSlider.position(10, height - 40);
+  redSlider.addClass("custom-slider");
+  greenSlider.addClass("custom-slider");
+  blueSlider.addClass("custom-slider");
+  weightSlider.addClass("custom-slider");
+  depthSlider.addClass("custom-slider");
+  angleSlider.addClass("custom-slider");
 
-  angleDisplay = createSpan();
-  angleDisplay.position(angleSlider.x + angleSlider.width + 10, height - 40);
 
 }
 function draw() {
-  clear();
+  if (clearEnabled) {
+    clear();
+  }
+  //clear();
   let r = redSlider.value();
   let g = greenSlider.value();
   let b = blueSlider.value();
   let strokeWidth = weightSlider.value();
   let depth = depthSlider.value();
-  
+
   stroke(r, g, b, 50);
   translate(width / 2, height / 2);
-  
+
   let newAng = angleSlider.value();
-  angleDisplay.html('Current angle: ' + newAng.toFixed(2));
-  
+  let angleInDegrees = degrees(newAng);
+  angleDisplay.html('Current angle: ' + angleInDegrees.toFixed(2));
+
   push();
   let weight = decreasingWeight ? strokeWidth * (depth / 10) : strokeWidth;
   let treeBranch = new Branch(100, depth, newAng, weight);
   treeBranch.draw();
   pop();
-  }
-  
-  function mousePressed() {
+}
+
+function mousePressed() {
   //exportSVG();
-  }
-  
-  function exportSVG() {
+}
+
+function exportSVG() {
   save("mySVG.svg"); // give file name
   print("saved svg");
   //noLoop(); // we just want to export once
-  }
+}
 
 // let angleSlider;
 // let angleDisplay;
